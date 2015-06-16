ad9e8a909348e4440d59cfbc5088dd7ebed8ec9b
Dan Stevens <stevens@earthit.com>
2015-06-16 13:44:47 -0500

    Only test for JSON UTF-8 encoding errors on versions of PHP that cause them.

--- test/EarthIT/JSONTest.php
+++ test/EarthIT/JSONTest.php
@@ -6,6 +6,11 @@ class EarthIT_JSONTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('"hi"', EarthIT_JSON::encode('hi'));
 	}
 	public function testEncodeError() {
+		if( PHP_VERSION_ID < 50500 ) {
+			$this->markTestSkipped("PHP < 5.5 doesn't throw errors when json_encode is asked to encode a malformed string.");
+			return;
+		}
+		
 		$caught = false;
 		try {
 			EarthIT_JSON::encode(substr("☹",0,1));
